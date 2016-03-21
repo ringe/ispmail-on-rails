781cec2d2bbeeb4b9795c59715569f456162ba5d
Runar Ingebrigtsen <runar@rin.no>
2016-03-21 14:40:48 +0100

    install clearance for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160321130353) do
+ActiveRecord::Schema.define(version: 20160321133546) do
 
   create_table "domains", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "email",              limit: 255, null: false
+    t.string   "encrypted_password", limit: 128, null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128, null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
   create_table "virtual_aliases", force: :cascade do |t|
     t.integer  "domain_id",   limit: 4,   null: false
