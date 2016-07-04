cad2468a9cd4814d1966c8a20f9019c422ed67f2
Runar Ingebrigtsen <runar@rin.no>
2016-07-04 23:41:30 +0200

    unique alias is the combination of source and destination

--- app/models/virtual_alias.rb
+++ app/models/virtual_alias.rb
@@ -1,6 +1,6 @@
 class VirtualAlias < ActiveRecord::Base
   validates :source, presence: true, domain_name: true, email: true
-  validates :destination, presence: true, alias: true, uniqueness: true
+  validates :destination, presence: true, alias: true
   validates :domain_id, presence: true
 
   belongs_to :virtual_domain, foreign_key: :domain_id
@@ -14,7 +14,7 @@ class VirtualAlias < ActiveRecord::Base
   def complete_emails
     self.source += "@#{virtual_domain.name}" unless source =~ /@/
     self.destination += "@#{virtual_domain.name}" unless destination =~ /@/
-    self.destination = destination.split(/,| /).select {|ds| !ds.empty? }.map(&:strip).join(", ")
+    self.destination = destination.split(/,| /).select {|ds| !ds.empty? }.map(&:strip).sort.join(", ")
   end
 
 end
--- app/validators/alias_validator.rb
+++ app/validators/alias_validator.rb
@@ -1,11 +1,13 @@
 # Validates that a string contains only valid email addresses, separated by comma
 class AliasValidator < EmailValidator
   def validate_each(record, attribute, value)
-    destinations = value.split(",")
-    unless destinations.map {|destination|
-        self.class.valid? destination.strip
-      }.all?
+    destinations = value.split(", ")
+    unless destinations.map {|destination| self.class.valid?(destination) }.all?
       record.errors[attribute] << (options[:message] || "is not a valid list of email addresses")
+    end
+    existing = VirtualAlias.where(source: record.source, destination: record.destination).count
+    if existing != 0
+      record.errors[attribute] << (options[:message] || "is already present")
     end
   end
 end
